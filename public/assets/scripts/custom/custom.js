9409c53f035429b7cb8d0516a1e90c814883bd6f
chandrakantpaliwal <chnadrakantpaliwal@Chnadras-MacBook-Air.local>
2014-08-21 11:17:53 +0530

    conflict file commit

--- public/assets/scripts/custom/custom.js
+++ public/assets/scripts/custom/custom.js
@@ -114,10 +114,9 @@ function selectedValue(){
         if($(this).val()==""){
             dom.parent().parent().find('.item-name').val("");
             dom.parent().parent().find('.item-weight').val("");
-            dom.parent().parent().find('.hidden_single_weight').val("");
+            dom.parent().parent().find('.actual-weight').val("");
             dom.parent().parent().find('.item-price').val("");
             dom.parent().parent().find('.actual-price').val("");
-            // dom.parent().parent().find('.hidden_original_price').val(data.items[0].price);
             dom.parent().parent().find('.item-quantity').val("");
             dom.parent().parent().find('.available-quantity').val("");
         }
@@ -129,9 +128,10 @@ function selectedValue(){
                         bootbox.alert("Quantity not available");
                         return;
                     }
+                    dom.parent().find('.ids').val(data.items[0].id);
                     dom.parent().parent().find('.item-name').val(data.items[0].name);
                     dom.parent().parent().find('.item-weight').val(data.items[0].weight);
-                    dom.parent().parent().find('.hidden_single_weight').val(data.items[0].weight);
+                    dom.parent().parent().find('.actual-weight').val(data.items[0].weight);
                     var price=0;
                     var total_price=0;
                     if(data.items[0].making_charges>0){
@@ -166,21 +166,24 @@ function validateQuantity(){
             $(this).val(1);
         }
         else{
-            console.log("yoyoyoyo");
             if($(this).parent().parent().find('.actual-price').attr('making_charges')=="yes"){
                 console.log("making charges yes");
                 var price=$(this).parent().parent().find('.actual-price').val();
-                var weight=$(this).parent().parent().find('.item-weight').val();
+                var weight=$(this).parent().parent().find('.actual-weight').val();
+                $(this).parent().parent().find('.item-weight').val(parseFloat($(this).val())*parseFloat(weight));
                 var total_price=parseFloat(price)*parseFloat(weight)*parseFloat($(this).val());
                 $(this).parent().parent().find('.item-price').val(total_price);
                 calculate_total_price();
             }
             else{
+                var weight=$(this).parent().parent().find('.actual-weight').val();
+                $(this).parent().parent().find('.item-weight').val(parseFloat($(this).val())*parseFloat(weight));
                 var price=$(this).parent().parent().find('.actual-price').val();
                 var total_price=parseFloat(price)*parseFloat($(this).val());
                 $(this).parent().parent().find('.item-price').val(total_price);
                 calculate_total_price();
             }
+            $('.discount-percent').blur();
         }
     });
 }
@@ -260,7 +263,91 @@ $(document).ready(function(){
         }
     });
     $('.generate_bill').click(function(){
-        $('form').submit();
+        $(".generate_bill").html("Bill Generating...");
+        var val = $("#form-bill").serialize();
+        $.post("/bill/",val, function(data) {
+            if(data.success == "true"){
+                $(".generate_bill").html("Bill Generated");
+                bootbox.alert(data.message);
+                window.location.replace("/");
+            } else {
+                bootbox.alert(data.message);
+                $(".generate_bill").html("Generate Bill");
+            }
+        });
     });
 });
+function initialize(){
+    // ComponentsPickers.init();
+    $(function() {
 
+    $.extend($.tablesorter.themes.bootstrap, {
+        // these classes are added to the table. To see other table classes available,
+        // look here: http://twitter.github.com/bootstrap/base-css.html#tables
+        table      : 'table table-bordered',
+        header     : 'bootstrap-header', // give the header a gradient background
+        footerRow  : '',
+        footerCells: '',
+        icons      : '', // add "icon-white" to make them white; this icon class is added to the <i> in the header
+        sortNone   : 'bootstrap-icon-unsorted',
+        sortAsc    : 'fa fa-chevron-up',
+        sortDesc   : 'fa fa-chevron-down',
+        active     : '', // applied when column is sorted
+        hover      : '', // use custom css here - bootstrap class may not override it
+        filterRow  : '', // filter row class
+        even       : '', // odd row zebra striping
+        odd        : ''  // even row zebra striping
+    });
+
+    // call the tablesorter plugin and apply the uitheme widget
+    $(".tablesorter").tablesorter({
+        // this will apply the bootstrap theme if "uitheme" widget is included
+        // the widgetOptions.uitheme is no longer required to be set
+        theme : "bootstrap",
+        widthFixed: true,
+        dateFormat : "ddmmyyyy",
+
+        headerTemplate : '{content} {icon}', // new in v2.7. Needed to add the bootstrap icon!
+
+        // widget code contained in the jquery.tablesorter.widgets.js file
+        // use the zebra stripe widget if you plan on hiding any rows (filter widget)
+        widgets : [ "uitheme", "filter", "zebra" ],
+
+        widgetOptions : {
+            // using the default zebra striping class name, so it actually isn't included in the theme variable above
+            // this is ONLY needed for bootstrap theming if you are using the filter widget, because rows are hidden
+            zebra : ["even", "odd"],
+
+            // reset filters button
+            filter_reset : ".reset"
+
+            // set the uitheme widget to use the bootstrap theme class names
+            // this is no longer required, if theme is set
+            // ,uitheme : "bootstrap"
+
+        },
+        headers: {
+            0:{
+                // disable it by setting the property sorter to false
+                sorter: false
+            }
+        }
+        }).tablesorterPager({
+
+            // target the pager markup - see the HTML block below
+            container: $(".ts-pager"),
+
+            // target the pager page select dropdown - choose a page
+            cssGoto  : ".pagenum",
+
+            // remove rows from the table to speed up the sort of large tables.
+            // setting this to false, only hides the non-visible rows; needed if you plan to add/remove rows with the pager enabled.
+            removeRows: false,
+
+            // output string - default is '{page}/{totalPages}';
+            // possible variables: {page}, {totalPages}, {filteredPages}, {startRow}, {endRow}, {filteredRows} and {totalRows}
+            output: '{startRow} - {endRow} / {filteredRows} ({totalRows})'
+
+        });
+    });
+}
